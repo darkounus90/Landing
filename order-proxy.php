71dafbc4cf003acc22146b05609ee9b701b350e9
Andres <mr608040@gmail.com>
2026-03-20 13:50:46 -0500

    fix: corregir error ortográfico en URL de Dropi (woocommerce)

--- order-proxy.php
+++ order-proxy.php
@@ -6,7 +6,7 @@
 
 // 1. Configuración
 $dropiToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcHAuZHJvcGkuY286ODAiLCJpYXQiOjE3NzQwMjc2NTcsImV4cCI6NDkyOTcwMTI1NywibmJmIjoxNzc0MDI3NjU3LCJqdGkiOiJQN0ZZS2VKb0tXWTk1V09FIiwic3ViIjo3MDAyNTksInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEiLCJhdWQiOiJXT09DT01FUkNFIiwidG9rZW5fdHlwZSI6IklOVEVHUkFUSU9OUyIsIndiX2lkIjoxLCJpbnRlZ3JhdGlvbl90eXBlIjoiV09PQ09NRVJDRSIsImludGVncmF0aW9uX3R5cGVfaWQiOjEsImlwX3VybCI6W10sImludGVncmF0aW9uX3VybCI6Imh0dHBzOlwvXC92aWRhc2FuYXMub25saW5lIn0.7gF1fPI-rY4TGjkJbrT8KTdbZ4Bp4FJG2rv_qH6TPJc';
-$apiUrl     = 'https://api.dropi.co/api/integrations/woocomerce/orders';
+$apiUrl     = 'https://api.dropi.co/api/integrations/woocommerce/orders';
 
 // 2. Obtener datos del POST (JSON)
 $inputJSON = file_get_contents('php://input');
